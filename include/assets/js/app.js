6a32cd5b7f1fadc81ab52ad96debdab5be8a8ff6
Lucas Jahn <luceoon@gmail.com>
2021-03-18 14:24:25 +0100

    Update v3
    Added Options Menu
    - Hide Code Panel
    - Darkmode

--- include/assets/js/app.js
+++ include/assets/js/app.js
@@ -1,4 +1,72 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  hljs.highlightAll();
 
-    hljs.highlightAll();
+  if (getCookie("show-code") == "false") {
+    $("#show-code-option-check").prop("checked", false);
+    $("#code").css("margin-left", "100%");
+    $("#canvas").css("width", "100%");
+  }
+
+  if (getCookie("darkmode") == "true") {
+    $("#darkmode-option-check").prop("checked", true);
+    $("#content").css("background-color", "#1e1e1e");
+    $('link[href="include/assets/css/code.css"]').attr(
+        "href",
+        "include/assets/css/code-darkmode.css"
+      );
+  }
+
+  $("#show-code-option").click(function () {
+    if ($("#show-code-option-check").is(":checked")) {
+      $("#code").animate({ marginLeft: "66.67%" }, 500);
+      $("#canvas").animate({ width: "66.67%" }, 500);
+      setCookie("show-code", "true", 365);
+    } else {
+      $("#code").animate({ marginLeft: "100%" }, 500);
+      $("#canvas").animate({ width: "100%" }, 500);
+      setCookie("show-code", "false", 365);
+    }
+  });
+
+  $("#darkmode-option").click(function () {
+    if ($("#darkmode-option-check").is(":checked")) {
+      $("#content").css("background-color", "#1e1e1e");
+      $('link[href="include/assets/css/code.css"]').attr(
+        "href",
+        "include/assets/css/code-darkmode.css"
+      );
+      setCookie("darkmode", "true", 365);
+    } else {
+      $("#content").css("background-color", "#ffffff");
+      $('link[href="include/assets/css/code-darkmode.css"]').attr(
+        "href",
+        "include/assets/css/code.css"
+      );
+      setCookie("darkmode", "false", 365);
+    }
+  });
 });
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(";");
+
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
--- index.php
+++ index.php
@@ -24,6 +24,8 @@
 **      SOFTWARE.
 */
 
+include 'include/functions.php';
+
 $baseUri = explode('?', $_SERVER['REQUEST_URI'], 2)[0];
 $fileExists = false;
 $filename = '';
@@ -37,15 +39,7 @@ if (isset($_GET['s']))
 
     if (isset($_GET['download']) && $fileExists)
     {
-        header('Content-Description: File Transfer');
-        header('Content-Type: application/octet-stream');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate');
-        header('Pragma: public');
-        header('Content-Disposition: attachment; filename="'.$sketchname.'.js"');
-        header('Content-Length: ' . filesize($filename));
-        readfile($filename);
-        exit;
+        downloadSketch($filename);
     }
 }
 ?>
@@ -57,16 +51,16 @@ if (isset($_GET['s']))
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/x-icon" href="include/assets/img/favicon.ico">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.6/styles/default.min.css">
+    <link rel="stylesheet" href="include/assets/css/code.css">
     <link rel="stylesheet" href="include/assets/css/style.css">
     <title><?php if($fileExists) echo 'P5 Sketch - '.$sketchname; else echo 'P5 Sketches'; ?></title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.6.0/highlight.min.js" integrity="sha512-zol3kFQ5tnYhL7PzGt0LnllHHVWRGt2bTCIywDiScVvLIlaDOVJ6sPdJTVi0m3rA660RT+yZxkkRzMbb1L8Zkw==" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.3.0/p5.min.js" integrity="sha512-tGZFF1kxT/c9C+kv77mKkZ9Ww1VyU8TMX6HLUSzbPrDLuptbiRFBfti8A33ip+BBIHYUsybuZD9OKLmIqdLmaQ==" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.3.0/addons/p5.sound.min.js" integrity="sha512-wM+t5MzLiNHl2fwT5rWSXr2JMeymTtixiw2lWyVk1JK/jDM4RBSFoH4J8LjucwlDdY6Mu84Kj0gPXp7rLGaDyA==" crossorigin="anonymous"></script>
-    <?php if ($fileExists): ?>
     <script src="include/assets/js/app.js"></script>
-    <script src="<?php echo $filename ?>"></script>
+    <?php if ($fileExists): ?>
+    <script src="<?php echo $filename.'?cache='.uniqid(); ?> ?>"></script>
     <?php endif ?>
 </head>
 <body>
@@ -77,26 +71,50 @@ if (isset($_GET['s']))
         <div id="title" class="header-item">
             <h1><?php if($fileExists) echo $sketchname; else echo 'P5 Sketches'; ?></h1>
         </div>
-        <div class="header-item"></div>
-    </div>
-
-    <?php if ($fileExists): ?>
-    <div>
-        <div id="canvas">
-            
+        <div class="header-item">
+            <div class="dropdown">
+                <button class="dropbtn">Optionen</button>
+                <div class="dropdown-content">
+                    <table>
+                        <tr>
+                            <td>
+                                <label id="show-code-option" class="switch">
+                                    <input id="show-code-option-check" type="checkbox" checked>
+                                    <span class="slider round"></span>
+                                </label>
+                            </td>
+                            <td class="slider-title">Code anzeigen</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label id="darkmode-option" class="switch">
+                                    <input id="darkmode-option-check" type="checkbox">
+                                    <span class="slider round"></span>
+                                </label>
+                            </td>
+                            <td class="slider-title">Darkmode</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
         </div>
-        <div id="code">
-            <pre><code class="js"><?php echo str_replace('{{$link}}', '<a href="'.$baseUri.'?s='.$sketchname.'&download">Download</a> ('.round(filesize($filename) / 1024, 2).' KiB)', file_get_contents('include/assets/js/code-header.js')).file_get_contents($filename); ?></code></pre>
-        </div>
     </div>
-    <?php else: ?>
-    <div id="list">
-        <?php foreach(glob("sketches/*.js") as $sketch):
-            $sketchname = basename($sketch, '.js');    
-        ?>
-            <a href="<?php echo $baseUri.'?s='.$sketchname ?>"><p><?php echo $sketchname.'.js' ?></p></a>
-        <?php endforeach; ?>
+    <div id="content">
+        <?php if ($fileExists): ?>
+            <div id="canvas"></div>
+            <div id="code">
+                <pre><code class="js"><?php renderCodeView($baseUri, $filename); ?></code></pre>
+            </div>
+        
+            <?php else: ?>
+            <div id="list">
+                <?php foreach(glob("sketches/*.js") as $sketch):
+                    $sketchname = basename($sketch, '.js');    
+                ?>
+                    <a href="<?php echo $baseUri.'?s='.$sketchname ?>"><p><?php echo $sketchname.'.js' ?></p></a>
+                <?php endforeach; ?>
+            </div>
+        <?php endif ?>
     </div>
-    <?php endif ?>
 </body>
 </html>
